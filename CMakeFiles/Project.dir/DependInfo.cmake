9e35b5bcb89a2d4824b74c8943280278e642f74a
vish <vishweshvhavle@gmail.com>
2022-12-04 13:40:13 +0530

    Implemented Bunny Mesh

---
+++ CMakeFiles/Project.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vish/solid_texture_synthesis/depends/imgui/imgui.cpp" "CMakeFiles/Project.dir/depends/imgui/imgui.cpp.o" "gcc" "CMakeFiles/Project.dir/depends/imgui/imgui.cpp.o.d"
+  "/home/vish/solid_texture_synthesis/depends/imgui/imgui_demo.cpp" "CMakeFiles/Project.dir/depends/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/Project.dir/depends/imgui/imgui_demo.cpp.o.d"
+  "/home/vish/solid_texture_synthesis/depends/imgui/imgui_draw.cpp" "CMakeFiles/Project.dir/depends/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/Project.dir/depends/imgui/imgui_draw.cpp.o.d"
+  "/home/vish/solid_texture_synthesis/depends/imgui/imgui_impl_glfw.cpp" "CMakeFiles/Project.dir/depends/imgui/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/Project.dir/depends/imgui/imgui_impl_glfw.cpp.o.d"
+  "/home/vish/solid_texture_synthesis/depends/imgui/imgui_impl_opengl3.cpp" "CMakeFiles/Project.dir/depends/imgui/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/Project.dir/depends/imgui/imgui_impl_opengl3.cpp.o.d"
+  "/home/vish/solid_texture_synthesis/depends/imgui/imgui_widgets.cpp" "CMakeFiles/Project.dir/depends/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/Project.dir/depends/imgui/imgui_widgets.cpp.o.d"
+  "/home/vish/solid_texture_synthesis/src/main.cpp" "CMakeFiles/Project.dir/src/main.cpp.o" "gcc" "CMakeFiles/Project.dir/src/main.cpp.o.d"
+  "/home/vish/solid_texture_synthesis/src/utils.cpp" "CMakeFiles/Project.dir/src/utils.cpp.o" "gcc" "CMakeFiles/Project.dir/src/utils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
