b68a8773a80013e7bf287c9fb2585195a4f333f1
vish <vishweshvhavle@gmail.com>
2022-12-07 22:05:19 +0530

    Added Documentation

--- src/texture.h
+++ src/texture.h
@@ -13,6 +13,8 @@ double compare(Mat patch1, Mat patch2)
     int width = patch1.size().width;
     Mat difference;
 
+    // Calculate Mean Squared Error between the Patches to measure similarity
+
     subtract(patch1, patch2, difference);
     difference = difference.mul(difference);
     double err = sum(difference)[0];
@@ -47,47 +49,39 @@ int stocastic_texture_synthesis(const char* texture_file)
     // cout << "Patch Width  = " << original_texture.size().width << endl << endl;
     // cout << "Patch height = " << original_texture.size().height << endl << endl;
 
-    Mat3b result_texture(rows, cols, Vec3b(0,0,0));
-    srand(time(0));
+    Mat3b result_texture(rows, cols, Vec3b(0,0,0)); // Create Empty Result Texture to load the Patches
+    srand(time(0)); // Make sure to generate new random values each 
 
-    
-    // imshow("Small Texture", small_texture);
-
-    Mat3b large_texture(rows, cols, Vec3b(0,0,0));
-    vector<Mat3b> patches(rows);
+    vector<Mat3b> patches(rows); //Store Patches
 
     for(int i=0; i<rows; i+=50){
         for(int j=0; j<cols; j+=50){
             double err = 50;
 
-            neighbour_x = rand() % (original_texture.size().width);
+            neighbour_x = rand() % (original_texture.size().width); //Select a random neighbourhood
             neighbour_y = rand() % (original_texture.size().height);
             
             int count = 1;
             while(err > 9.8){
-                random_x = neighbour_x - 75 + rand() % 150;
+                random_x = neighbour_x - 75 + rand() % 150; //Select a patch from the neighbourhood
                 random_y = neighbour_y - 75 + rand() % 150;
-                // cout << "Random X = " << random_x << endl << endl;
-                // cout << "Random Y = " << random_y << endl << endl;
                 if((random_x <= 25 or (random_x >= (original_texture.size().width - 25)))
                     or (random_y <= 25 or (random_y >= (original_texture.size().height - 25)))) continue;
                 else{
-                    // cout << "Random X = " << random_x << endl << endl;
-                    // cout << "Random Y = " << random_y << endl << endl;
                     patch = original_texture(Range(random_y-25,random_y+25), Range(random_x-25,random_x+25));
                     if(flag) err = compare(prev_patch, patch);
                     else err = 0;
                 }
                 if(count++ % 10){
-                    neighbour_x = rand() % (original_texture.size().width);
+                    neighbour_x = rand() % (original_texture.size().width); //Ensure we don't get stuck in local minima
                     neighbour_y = rand() % (original_texture.size().height);
                 } 
             }
                 
-            patch.copyTo(result_texture(Rect(i, j, patch.cols, patch.rows)));
+            patch.copyTo(result_texture(Rect(i, j, patch.cols, patch.rows))); // Add patch to final result
             patches[j]=patch; 
             imwrite("./texture/output.jpg", result_texture);
-            prev_patch = patch;
+            prev_patch = patch; //Store previous patch for comparision
             flag = true;
         }
     }
@@ -95,14 +89,14 @@ int stocastic_texture_synthesis(const char* texture_file)
     for(int i=50; i<rows; i+=50){
         for(int j=50; j<cols; j+=50){
             patch = result_texture(Range(j-20, j+20), Range(i-20, i+20));
-            GaussianBlur(patch, patch, Size(5,5),0);
+            GaussianBlur(patch, patch, Size(5,5),0); // Add Gaussian Blur at seams to improve seamlessness
             patch.copyTo(large_texture(Rect(i-20, j-20, patch.cols, patch.rows)));
         }
     }
     
-    GaussianBlur(result_texture, result_texture, Size(3,3),0);
+    GaussianBlur(result_texture, result_texture, Size(3,3),0); // Add Gaussian Blur 
 
-    imshow("Result Texture", result_texture);
+    imshow("Result Texture", result_texture); // Display Final Texture
     imwrite("./texture/output.jpg", result_texture);
     waitKey(0);
     destroyAllWindows();
